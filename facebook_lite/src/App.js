25015addac3aadff9772c3848e24697e7e7c3142
LouisBousiquier <louis.bousiquier@edu.ece.fr>
2019-11-06 23:04:11 +0100

    affichage photo et recup données entre components

--- facebook_lite/src/App.js
+++ facebook_lite/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import Profil from "./Profil.js";
 import './App.css';
+import { createRequire } from 'module';
 
 class App extends React.Component{
   render(){
     return(
       <div>
-        <Profil prenom="Louis"/>
+        <Profil prenom="Louis" nom="Bousiquier" image={require("./pp.png")} date="5 févier 1997" commentaire="qlsdicglqsidugdl"/>
+
       </div>
     );
   }
