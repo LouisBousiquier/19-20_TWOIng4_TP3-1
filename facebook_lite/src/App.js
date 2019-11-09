08a79426789be6566d2245e05d2bfa3a1ad322be
LouisBousiquier <louis.bousiquier@edu.ece.fr>
2019-11-09 22:38:26 +0100

    changer de profil en cliquant

--- facebook_lite/src/App.js
+++ facebook_lite/src/App.js
@@ -1,18 +1,115 @@
 import React from 'react';
 import logo from './logo.svg';
-import Profil from "./Profil.js";
+//import Profil from "./Profil.js";
 import './App.css';
 import { createRequire } from 'module';
+import {Nav, NavItem, NavLink} from 'reactstrap';
 
-class App extends React.Component{
+
+class Profil extends React.Component{
+  constructor(props){
+      super(props);
+      this.state= {
+          val: null,
+          liked : false,
+          likeCounter :0,
+          user : [
+              {
+                  prenom : "blalzdjf",
+                  nom : "bousiquier" ,
+                  image : "./pp.png",
+                  date : "5 février 1997",
+                  commentaire : "ligl",
+                  like : 0
+              },
+              {
+                  prenom : "hdf",
+                  nom : "dosh",
+                  image : "./pp.png",
+                  date : "sdihf",
+                  commentaire : "idg",
+                  like : 0
+              },
+              {
+                  prenom : "khg",
+                  nom : "jh",
+                  image : "./pp.png",
+                  date : "jdzifh",
+                  commentaire : "jdfh",
+                  like : 0
+              }
+          ],
+          idUtilisateur : 0
+      };      
+      //this.handleClick= this.handleClick.bind(this);
+  }
+  handleClick(i){
+      this.setState({idUtilisateur : i})
+  }
+  //fonction pour liker (le "c'est super")
+  /*super = ()=>{
+      let likeCounter = this.state.likeCounter;
+      //likeCounter += !this.state.liked ? 1 : -1;
+      this.setState({
+          liked : !this.state.liked
+      })
+  }*/
   render(){
-    return(
-      <div>
-        <Profil prenom="Louis" nom="Bousiquier" image={require("./pp.png")} date="5 févier 1997" commentaire="qlsdicglqsidugdl"/>
+      //const {pers, publi, image}= this.props.profil;
 
-      </div>
-    );
+      return(
+         <body>
+          <header>
+              <div className="button">
+                      <button onClick={()=>this.handleClick(0)}> {this.state.user[0].prenom}</button>
+                      <button onClick={()=>this.handleClick(1)}> {this.state.user[1].prenom}</button>
+                      <button onClick={()=>this.handleClick(2)}> {this.state.user[2].prenom}</button>
+              </div>
+          </header>
+              
+              <fieldset id={"card"} className={' card d-flex align-items-center border'}>
+                  
+              
+              <div className="image">
+              <img src={this.state.user[this.state.idUtilisateur].image}></img>
+              </div>
+
+                  <article className={"info"}>
+                      <div className={"first_name"} >
+                          <p>{this.state.user[this.state.idUtilisateur].prenom}</p>
+                          
+                      </div>
+                      
+                      
+                      <div className={"last_name"}>
+                          <p>{this.state.user[this.state.idUtilisateur].nom}</p>
+                      </div>
+                  </article>
+
+                  <div className={"date"}>
+                         <p> {this.state.user[this.state.idUtilisateur].date}</p>
+                  </div>
+
+                  <div>
+                      <button className={'style'}>Change style!</button>
+                  </div>
+
+              </fieldset>
+
+          <fieldset className={'com'}>
+              <article className={'t_com'}>
+                  <p>
+                      {this.state.user[this.state.idUtilisateur].commentaire}
+                  </p>
+              </article>
+              <button className={'super'} id={"super"}>
+                  C'est super !
+              </button>
+
+          </fieldset>
+              </body>
+      );
   }
 }
 
-export default App;
+export default Profil;
