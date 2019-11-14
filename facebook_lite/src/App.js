9fa0794aadba180e8907847d838dc0c72b6e777a
LouisBousiquier <louis.bousiquier@edu.ece.fr>
2019-11-14 20:09:25 +0100

    style sur boutons, fct super en cours

--- facebook_lite/src/App.js
+++ facebook_lite/src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
-//import Profil from "./Profil.js";
 import './App.css';
-//import { createRequire } from 'module';
-import {Nav, NavItem, NavLink} from 'reactstrap';
 import pp from './img/pp.png';
 
 class Bouttons extends React.Component {
   render() {
       return(
-          <button onClick={this.props.onClick} > {this.props.idbutton} </button>
+          <button type="button" id="bt" style={{backgroundColor: 'blue' , margintop: '7%', paddingTop:'15%',paddingBottom:'15%', font:'Bold 13px Arial', borderRadius:'12px 0 12px 0', paddingInlineStart: '15%', paddingInlineEnd:'15%', marginInlineStart:'7%',marginInlineEnd:'7%'}} onClick={this.props.onClick} > {this.props.idbutton} </button>
       );
   }
 }
@@ -17,6 +14,7 @@ class Bouttons extends React.Component {
 class Profil extends React.Component{
   constructor(props){
       super(props);
+      this.super = this.super.bind(this);
       this.state= {
         
           val: null,
@@ -27,9 +25,10 @@ class Profil extends React.Component{
                   image : pp,
                   date : "5 février 1997",
                   commentaire : "ligl",
-                  like : 0,
                   background : "white",
-                  font : "black"
+                  color : "black",
+                  //liked : false,
+                  //likeCounter :0,
               },
               {
                   prenom : "hdf",
@@ -37,9 +36,10 @@ class Profil extends React.Component{
                   image : "./pp.png",
                   date : "sdihf",
                   commentaire : "idg",
-                  like : 0,
                   background : "white",
-                  font : "black"
+                  font : "black",
+                  liked : false,
+                  likeCounter :0,
               },
               {
                   prenom : "khg",
@@ -47,9 +47,10 @@ class Profil extends React.Component{
                   image : "./pp.png",
                   date : "jdzifh",
                   commentaire : "jdfh",
-                  like : 0,
                   background : "white",
-                  font : "black"
+                  font : "black",
+                  liked : false,
+                  likeCounter :0,
               }
           ],
           idUtilisateur : 0
@@ -59,31 +60,37 @@ class Profil extends React.Component{
   handleClick(i){
       this.setState({idUtilisateur : i})
   }
-  changeBackground(){
+  changeBackground() {
     if (this.state.background === "white"){
       this.setState(
-        state => ({background : "blue" 
-      }))
+        state => ({background : "black" , color: "white"
+      }));
       //alert(this.state.user[this.state.idUtilisateur].background)
       //{alert(this.state.idUtilisateur)}
-    } else{
+    } else {
       this.setState(
-        state => ({background : "white" 
-      }))
+        state => ({background : "white" , color: "black"
+      }));
       //alert(this.state.user[this.state.idUtilisateur].background)
     }
-    
   }
-  render(){
-      //const {pers, publi, image}= this.props.profil;
 
+  //fonction pour liker (le "c'est super")
+  super(){
+    let likeCounter = this.state.likeCounter;
+   // likeCounter += !this.state.liked ? 1 : -1
+   this.setState({
+     likeCounter : likeCounter +1
+   }) 
+}
+  render(){
       return(
          <body>
           <header>
-            <div className="button">
-              <Bouttons idbutton={this.state.user[0].prenom} onClick={() => this.handleClick(0)}/>
-              <Bouttons idbutton={this.state.user[1].prenom} onClick={() => this.handleClick(1)}/>
-              <Bouttons idbutton={this.state.user[2].prenom} onClick={() => this.handleClick(2)}/>
+            <div className="buttons d-flex">
+              <Bouttons className="b1" idbutton={this.state.user[0].prenom} onClick={() => this.handleClick(0)}/>
+              <Bouttons  idbutton={this.state.user[1].prenom} onClick={() => this.handleClick(1)}/>
+              <Bouttons  idbutton={this.state.user[2].prenom} onClick={() => this.handleClick(2)}/>
             </div>
           </header>
               
@@ -96,32 +103,33 @@ class Profil extends React.Component{
 
                   <article className={"info"} >
                       <div className={"first_name"} >
-                          <p>{this.state.user[this.state.idUtilisateur].prenom}</p>
+                          <p style={{color : this.state.color}}>{this.state.user[this.state.idUtilisateur].prenom}</p>
                       </div>
 
                       <div className={"last_name"}>
-                          <p>{this.state.user[this.state.idUtilisateur].nom}</p>
+                          <p style={{color : this.state.color}}>{this.state.user[this.state.idUtilisateur].nom}</p>
                       </div>
                   </article>
 
                   <div className={"date"}>
-                         <p> {this.state.user[this.state.idUtilisateur].date}</p>
+                         <p style={{color : this.state.color}}> {this.state.user[this.state.idUtilisateur].date}</p>
                   </div>
 
                   <div>
-                      <button className={'style'} onClick={() => this.changeBackground()} >
+                      <button className={'style'} onClick={() => this.changeBackground()}>
                         
                         Change style!</button>
                   </div>
 
               </fieldset>
 
-          <fieldset className={'com'}>
+          <fieldset className={'com'} style={{background: this.state.background, color : this.state.color}}>
               <Com 
               comm={this.state.user[this.state.idUtilisateur].commentaire}
-              like={this.state.user[this.state.idUtilisateur].like}
-             // onClick={() =>this.state.super(this.state.idUtilisateur) }
               />
+              <button className="super" id="super" onClick={() => this.super()} >
+          c'est super</button>
+          <p style={{color: 'red'}}>{this.state.like}lk</p>
           </fieldset>
 
               </body>
@@ -130,22 +138,6 @@ class Profil extends React.Component{
 }
 
 class Com extends React.Component{
-  constructor(props){
-    super(props);
-    this.sate={
-      liked : false,
-      likeCounter :0
-    }
-  }
-    //fonction pour liker (le "c'est super")
-  super(i){
-      let likeCounter = this.state.likeCounter;
-      likeCounter += !this.state.liked ? 1 : -1
-      this.setState({
-          liked : !this.state.liked,
-          likeCounter : likeCounter
-      })
-  }
 
   render(){
     return(
@@ -155,12 +147,6 @@ class Com extends React.Component{
             {this.props.comm}
           </p>     
         </article>
-
-        <button className="super" id="super" onClick={this.props.onClick} >
-          c'est super
-         
-        </button>
-        <span>{this.props.likeCounter}</span>
       </div>
     );
   }
